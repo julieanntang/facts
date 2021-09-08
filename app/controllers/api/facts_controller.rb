ded5a5676abab86fcf9aa673a99d8269d16dbdfd
Julie Tang <julieanntang@hotmail.com>
2021-09-08 00:51:30 -0600

    facts index

---
+++ app/controllers/api/facts_controller.rb
@@ -0,0 +1,7 @@
+class Api::FactsController < ApplicationController
+  
+  def index
+    render json: Fact.all
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Fact.create(text: "5g causes covid", stars:1, source:'google', username:"Larry")
+Fact.create(text: "World is flat", stars:1, source:'my eyes', username:"Larry")
+Fact.create(text: "Armadillos are bullet proof", stars:0, source:'internet', username:"Will")
+Fact.create(text: "Some fungi create zombies, then control their minds", stars:4.2, source:'https://www.nationalgeographic.com/animals/article/110511-zombies-ants-fungus-infection-spores-bite-noon-animals-science', username:"James")
+Fact.create(text: "Whales Swallow Half a Million Calories in Single Mouthful", stars:3.3, source:'https://www.nationalgeographic.com/animals/article/110511-zombies-ants-fungus-infection-spores-bite-noon-animals-science', username:"James")
+puts "seeded facts size: #{Fact.all.size}"
